b78597716a3866b6c6cd44f792be4af54e3753d9
NodeLoc.com <149086144+nodeloc@users.noreply.github.com>
2024-11-03 03:20:28 +0800

    Update AttachedPostPreviewer.tsx

--- js/src/forum/components/AttachedPostPreviewer.tsx
+++ js/src/forum/components/AttachedPostPreviewer.tsx
@@ -10,6 +10,7 @@ import ItemList from 'flarum/common/utils/ItemList';
 import Discussion from 'flarum/common/models/Discussion';
 import humanTime from 'flarum/common/helpers/humanTime';
 import Button from 'flarum/common/components/Button';
+import fullTime from 'flarum/common/helpers/fullTime';
 
 export default class AttachedPostPreviewer extends Component<{
     post: Post,
@@ -24,6 +25,8 @@ export default class AttachedPostPreviewer extends Component<{
         const maxLines = app.forum.attribute<number>('xypp-attached-posts.max-lines');
         const currentUser = app.session?.user;
         const postUser = this.attrs.post.user();
+        const post = this.attrs.post;
+        const time = post.createdAt();
         let canDelete = false;
         if (currentUser && postUser && currentUser.id() !== postUser.id()) {
             if (this.attrs.post.discussion().attribute("xypp-attached-posts-moderate")) {
@@ -33,43 +36,20 @@ export default class AttachedPostPreviewer extends Component<{
             canDelete = true;
         }
         return (
-            <div className="CommentPost">
-                <div className="Post-header">
-                    <div className="AttachTitle" onclick={() => app.current.get('stream').goToNumber(this.attrs.post.number())}>
-                        <i class="fas fa-paperclip"></i>
-                        {app.translator.trans("xypp-attached-posts.forum.attached")}
-                    </div>
-                    <div className="PostUser">
-                        {postUser && userOnline(postUser)}
-                        <h3>{postUser ? <Link href={app.route.user(postUser)}>{username(postUser)}</Link> : username(postUser)}</h3>
-                    </div>
+            <li>
+                <span class="time">{humanTime(time)}</span>
+                <span class="dot bg-success"></span>
+                <div class="content">
+                    <h3 class="subtitle">{postUser ? <Link href={app.route.user(postUser)}>{username(postUser)}</Link> : username(postUser)}</h3>
+                    <p>{m.trust(this.attrs.post.contentHtml())}</p>
                 </div>
-                <div className={classList('Post-body', maxLines > 0 && 'attached-post', (!this.showMore) && "truncate")} style={{ '--max-lines': maxLines }}>
-                    <div className='Post-body-wrapper'>
-                        {m.trust(this.attrs.post.contentHtml())}
-                    </div>
-                </div>
-                <div className="AttachedPost-footer" onclick={(() => { this.showMore = !this.showMore; m.redraw() }).bind(this)}>
-                    {this.showMore ? (
-                        [
-                            <i className="fas fa-angle-up" />,
-                            app.translator.trans("xypp-attached-posts.forum.show-less")
-                        ]
-                    ) : (
-                        [
-                            <i className="fas fa-angle-down" />,
-                            app.translator.trans("xypp-attached-posts.forum.show-more")
-                        ]
-                    )}
-                </div>
-
                 {canDelete ?
                     <Button className="attached-posts-delete Button" icon="fas fa-trash" onclick={this.unattach.bind(this)} loading={this.removing} disabled={this.removing}>
                         {app.translator.trans("xypp-attached-posts.forum.delete")}
                     </Button>
                     : ""
                 }
-            </div>
+            </li>
         );
     }
 
